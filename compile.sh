1f7941a43ba2c512669ffb6ae6545358ae81c405
Philip Zucker <philzook58@gmail.com>
2024-04-12 15:29:58 -0400

    abstract

---
+++ compile.sh
@@ -0,0 +1,5 @@
+pdflatex acmart-sigplanproc-template
+bibtex acmart-sigplanproc-template
+pdflatex acmart-sigplanproc-template
+pdflatex acmart-sigplanproc-template
+# seriously? This is what is reccommenned?
